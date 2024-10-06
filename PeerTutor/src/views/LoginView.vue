9a534e5962cfdf055a7c8efbc4109c40d4671952
Kayel2 <gulayjunkyle@gmail.com>
2024-10-07 01:25:57 +0800

    added vuetify

---
+++ PeerTutor/src/views/LoginView.vue
@@ -0,0 +1,34 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const theme = ref('light')
+
+  function onClick () {
+    theme.value = theme.value === 'light' ? 'dark' : 'light'
+  }
+</script>
+
+<template>
+    <v-responsive class="border rounded" max-height="300">
+      <v-app :theme="theme">
+        <v-app-bar class="px-3">
+          <v-spacer></v-spacer>
+  
+          <v-btn
+            :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+            text="Toggle Theme"
+            slim
+            @click="onClick"
+          ></v-btn>
+        </v-app-bar>
+  
+        <v-main>
+          <v-container>
+            <h1>Main Content</h1>
+          </v-container>
+        </v-main>
+      </v-app>
+    </v-responsive>
+
+    
+</template>
