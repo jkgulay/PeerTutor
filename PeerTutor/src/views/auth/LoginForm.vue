871fc64e89e18498e658796d71f055da67dc7290
Kayel2 <gulayjunkyle@gmail.com>
2024-10-14 20:28:11 +0800

    "created LoginForm and Register for separated form"

---
+++ PeerTutor/src/views/auth/LoginForm.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref } from 'vue'
+import { useField, useForm } from 'vee-validate'
+
+const { check } = useForm({
+  validationSchema: {
+    password(value) {
+      return /^[0-9-]{7,}$/.test(value) || 'Password needs to be at least 7 digits.'
+    },
+    email(value) {
+      return /^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value) || 'Must be a valid e-mail.'
+    }
+  }
+})
+
+// Form fields
+const visible = ref(false)
+const password = useField('password')
+const email = useField('email')
+const loading = ref(false)
+</script>
+
+<template>
+  <v-form fast-fail @submit.prevent>
+    <v-text-field
+      class="mx-3 my-2"
+      variant="outlined"
+      prepend-inner-icon="mdi-account"
+      density="compact"
+      :error-messages="email.errorMessage"
+      placeholder="user@gmail.com"
+      label="Email"
+      hide-details="auto"
+      clearable
+    ></v-text-field>
+
+    <v-text-field
+      class="mx-3 my-2"
+      density="compact"
+      variant="outlined"
+      prepend-inner-icon="mdi-lock"
+      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+      :type="visible ? 'text' : 'password'"
+      @click:append-inner="visible = !visible"
+      :error-messages="password.errorMessage"
+      label="Password"
+      placeholder="Enter your password"
+      hide-details="auto"
+      clearable
+    ></v-text-field>
+
+    <v-container width="200">
+      <v-btn
+        class=""
+        :loading="loading"
+        color="teal-darken-2"
+        size="large"
+        type="submit"
+        variant="elevated"
+        block
+        elevation="10"
+        style="border-radius: 30px"
+      >
+        <span style="color: #80cbc4">Sign In</span>
+      </v-btn></v-container
+    >
+  </v-form>
+</template>
--- PeerTutor/src/views/auth/RegisterView.vue
+++ PeerTutor/src/views/auth/RegisterView.vue
@@ -1,42 +1,6 @@
 <script setup>
 import AppLayout from '@/components/layout/AppLayout.vue'
-import { ref } from 'vue'
-import { useField, useForm } from 'vee-validate'
-
-const { handleSubmit } = useForm({
-  validationSchema: {
-    userName(value) {
-      return value?.length >= 2 || 'User Name needs to be at least 2 characters.'
-    },
-    password(value) {
-      return /^[0-9-]{7,}$/.test(value) || 'Password needs to be at least 7 digits.'
-    },
-    email(value) {
-      return /^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value) || 'Must be a valid e-mail.'
-    },
-    role(value) {
-      return value || 'Select a role.'
-    }
-  }
-})
-
-// Form fields
-const visible = ref(false)
-const repeatVisible = ref(false)
-
-const userName = useField('userName')
-const password = useField('password')
-const email = useField('email')
-const role = useField('role')
-
-const items = ref(['Student', 'Tutor'])
-const loading = ref(false)
-const repeatPassword = ref('')
-
-// Form submission
-const submit = handleSubmit((values) => {
-  alert(JSON.stringify(values, null, 2))
-})
+import RegisterForm from './RegisterForm.vue'
 </script>
 
 <template>
@@ -57,82 +21,7 @@ const submit = handleSubmit((values) => {
               <v-divider class="my-2"></v-divider>
             </template>
 
-            <v-form @submit.prevent="submit" fast-fail>
-              <v-text-field
-                class="mx-3 my-2"
-                variant="outlined"
-                prepend-inner-icon="mdi-account"
-                density="compact"
-                :error-messages="email.errorMessage"
-                placeholder="user@gmail.com"
-                label="Email"
-                hide-details="auto"
-                clearable
-              ></v-text-field>
-
-              <v-text-field
-                class="mx-3 my-2"
-                variant="outlined"
-                prepend-inner-icon="mdi-badge-account-outline"
-                density="compact"
-                :error-messages="userName.errorMessage"
-                placeholder="Enter Name"
-                label="User Name"
-                hide-details="auto"
-                clearable
-              ></v-text-field>
-
-              <v-text-field
-                class="mx-3 my-2"
-                density="compact"
-                variant="outlined"
-                prepend-inner-icon="mdi-lock"
-                :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-                :type="visible ? 'text' : 'password'"
-                @click:append-inner="visible = !visible"
-                :error-messages="password.errorMessage"
-                label="Password"
-                placeholder="Enter your password"
-                hide-details="auto"
-                clearable
-              ></v-text-field>
-
-              <v-text-field
-                class="mx-3 my-2"
-                density="compact"
-                variant="outlined"
-                prepend-inner-icon="mdi-lock-check"
-                :append-inner-icon="repeatVisible ? 'mdi-eye-off' : 'mdi-eye'"
-                :type="repeatVisible ? 'text' : 'password'"
-                @click:append-inner="repeatVisible = !repeatVisible"
-                label="Repeat Password"
-                placeholder="Repeat your password"
-                hide-details="auto"
-                clearable
-              ></v-text-field>
-
-              <v-select
-                class="mx-3"
-                :error-messages="role.errorMessage"
-                :items="items"
-                label="Role"
-              ></v-select>
-
-              <v-container width="200">
-                <v-btn
-                  :loading="loading"
-                  color="teal-darken-2"
-                  size="large"
-                  type="submit"
-                  variant="elevated"
-                  block
-                  elevation="10"
-                  style="border-radius: 30px"
-                >
-                  <span style="color: #80cbc4">Create</span>
-                </v-btn>
-              </v-container>
-            </v-form>
+            <RegisterForm></RegisterForm>
             <v-divider class="mx-3"></v-divider>
             <v-card-text class="text-center">
               <a class="text-white text-decoration-none" rel="noopener noreferrer" target="_blank">
