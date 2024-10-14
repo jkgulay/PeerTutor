a9c9c14a6b0e6c46aa0e3a6ca88317c4d594c30e
Kayel2 <gulayjunkyle@gmail.com>
2024-10-14 22:32:39 +0800

    ft: added vcontainers with fluid

--- PeerTutor/src/views/auth/LoginView.vue
+++ PeerTutor/src/views/auth/LoginView.vue
@@ -6,33 +6,40 @@ import LoginForm from './LoginForm.vue'
 <template>
   <AppLayout>
     <template #content>
-      <v-row class="dflex justify-center">
-        <v-col>
-          <v-card
-            class="mx-auto px-6"
-            max-width="450"
-            subtitle="PeerTutor"
-            style="margin: 30px; border-radius: 30px"
-            elevation="20"
-            color="#294D61"
-          >
-            <template v-slot:title>
-              <v-img class="mx-auto" max-width="228" src="/logo/try2.png"></v-img>
+      <v-container fluid="">
+        <v-row class="dflex justify-center">
+          <v-col>
+            <v-card
+              class="mx-auto px-6"
+              max-width="450"
+              subtitle="PeerTutor"
+              style="margin: 30px; border-radius: 30px"
+              elevation="20"
+              color="#294D61"
+            >
+              <template v-slot:title>
+                <v-img class="mx-auto" max-width="228" src="/logo/try2.png"></v-img>
 
-              <span class="font-weight-black text-teal-lighten-3">Welcome!</span>
-            </template>
+                <span class="font-weight-black text-teal-lighten-3">Welcome!</span>
+              </template>
 
-            <LoginForm></LoginForm>
+              <LoginForm></LoginForm>
 
-            <v-divider class="mx-3"></v-divider>
-            <v-card-text class="text-center">
-              <a class="text-white text-decoration-none" rel="noopener noreferrer" target="_blank">
-                Don't have an account? <RouterLink class="text-teal-lighten-3" to="/register">Sign up now</RouterLink
-                ><v-icon icon="mdi-chevron-right"></v-icon>
-              </a>
-            </v-card-text> </v-card
-        ></v-col>
-      </v-row>
+              <v-divider class="mx-3"></v-divider>
+              <v-card-text class="text-center">
+                <a
+                  class="text-white text-decoration-none"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Don't have an account?
+                  <RouterLink class="text-teal-lighten-3" to="/register">Sign up now</RouterLink
+                  ><v-icon icon="mdi-chevron-right"></v-icon>
+                </a>
+              </v-card-text> </v-card
+          ></v-col>
+        </v-row>
+      </v-container>
     </template>
   </AppLayout>
 </template>
--- PeerTutor/src/views/auth/RegisterView.vue
+++ PeerTutor/src/views/auth/RegisterView.vue
@@ -6,32 +6,39 @@ import RegisterForm from './RegisterForm.vue'
 <template>
   <AppLayout
     ><template #content
-      ><v-row class="dflex justify-center">
-        <v-col>
-          <v-card
-            class="mx-auto px-6"
-            max-width="450"
-            subtitle="PeerTutor"
-            style="margin: 30px; border-radius: 30px"
-            elevation="20"
-            color="#294D61"
-          >
-            <template v-slot:title>
-              <span class="font-weight-black text-teal-lighten-3">Create an account</span>
-              <v-divider class="my-2"></v-divider>
-            </template>
+      ><
+      <v-container fluid="">
+        ><v-row class="dflex justify-center">
+          <v-col>
+            <v-card
+              class="mx-auto px-6"
+              max-width="450"
+              subtitle="PeerTutor"
+              style="margin: 30px; border-radius: 30px"
+              elevation="20"
+              color="#294D61"
+            >
+              <template v-slot:title>
+                <span class="font-weight-black text-teal-lighten-3">Create an account</span>
+                <v-divider class="my-2"></v-divider>
+              </template>
 
-            <RegisterForm></RegisterForm>
-            <v-divider class="mx-3"></v-divider>
-            <v-card-text class="text-center">
-              <a class="text-white text-decoration-none" rel="noopener noreferrer" target="_blank">
-                Already have an account?<v-icon icon="mdi-chevron-left"></v-icon>
-                <RouterLink class="text-teal-lighten-3" to="/login">Sign In</RouterLink>
-              </a>
-            </v-card-text>
-          </v-card></v-col
-        >
-      </v-row></template
+              <RegisterForm></RegisterForm>
+              <v-divider class="mx-3"></v-divider>
+              <v-card-text class="text-center">
+                <a
+                  class="text-white text-decoration-none"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Already have an account?<v-icon icon="mdi-chevron-left"></v-icon>
+                  <RouterLink class="text-teal-lighten-3" to="/login">Sign In</RouterLink>
+                </a>
+              </v-card-text>
+            </v-card></v-col
+          >
+        </v-row></v-container
+      ></template
     >
   </AppLayout>
 </template>
