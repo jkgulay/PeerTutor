55443479f9f9129e6760d5081c69b371ec10d126
ccqg <gabaleschrist1@gmail.com>
2024-10-15 18:53:26 +0800

    Profile View

--- PeerTutor/src/router/index.js
+++ PeerTutor/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 import HomeView from '@/views/system/HomeView.vue'
+import ProfileView from '@/views/system/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +22,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView
+    }
   ]
 })
 
