25621a64c443373df44dee5ffc49ad88374e1e1e
Jun Kyle <110974133+jkgulay@users.noreply.github.com>
2024-11-11 22:51:05 +0800

    Development (#28)
    * fixed: re-edit some line error
    
    * feat: added some cards in HomeVue
    
    * feat: separated username: now first name and last name (#10)
    
    * fix: arranged the navmenu and the form in RegisterForm.Vue
    
    * fix: additional bugs
    
    * ft: added search bar
    
    * fix: renamed the search bar
    
    * fix: the nav drawer from scrolling down
    
    * fix: changed role in form
    
    * fix: added router link to login form signin button
    
    * fix: small tweaks to access home in vercel
    
    * fix: re-aligned some grid in register form and homeview
    
    * fix: small
    
    * feat: added edit profile dialog in profile
    
    * fix: minor things
    
    * feat: added availability toggle button in profile, added message box in contact tutor button, and added message dialog in message button in navbar
    
    * fix: fixed register form and added occupation
    
    * commit
    
    * fix: fixed some things
    
    * fix: rearranged the RegForm
    
    * fix: minor things
    
    * commit
    
    * merged

--- PeerTutor/src/views/auth/LoginForm.vue
+++ PeerTutor/src/views/auth/LoginForm.vue
@@ -1,11 +1,11 @@
 <script setup>
 import AlertNotification from '@/components/common/AlertNotification.vue'
-
 import { supabase, formActionDefault } from '@/utils/supabase'
 import { ref } from 'vue'
 import { requiredValidator, emailValidator } from '@/utils/validators'
 import { useRouter } from 'vue-router'
 
+// Router instance
 const router = useRouter()
 
 // Default form data
@@ -13,63 +13,49 @@ const formDataDefault = {
   email: '',
   password: ''
 }
-const formData = ref({
-  ...formDataDefault
-})
-const formAction = ref({
-  ...formActionDefault
-})
 
+// Reactive form data and action state
+const formData = ref({ ...formDataDefault })
+const formAction = ref({ ...formActionDefault })
+
+// Password visibility state
 const isPasswordVisible = ref(false)
+
+// Reference to the form
 const refVForm = ref()
 
 // Login function
 const onSubmit = async () => {
-  //Reset Form Action utils
+  // Reset form action state
   formAction.value = { ...formActionDefault }
   // Turn on processing
   formAction.value.formProcess = true
 
+  // Attempt to sign in with Supabase
   const { data, error } = await supabase.auth.signInWithPassword({
     email: formData.value.email,
     password: formData.value.password
   })
 
+  // Handle response
   if (error) {
     formAction.value.formErrorMessage = error.message
     formAction.value.formStatus = error.status
   } else if (data) {
-    formAction.value.formSuccessMessage = 'Sucessfully Login!'
-    router.replace('/home')
+    formAction.value.formSuccessMessage = 'Successfully Logged In!' // Corrected spelling
+    router.replace('/home') // Redirect to home
   }
 
-  //Reset Form
+  // Reset form
   refVForm.value?.reset()
-
-  //Turn off processing
+  // Turn off processing
   formAction.value.formProcess = false
-
-
-const isPasswordVisible = ref(false)
-const refVForm = ref()
-
-// Default form data
-const formData = ref({
-  email: '',
-  password: ''
-})
-
-// Login function
-const onLogin = () => {
-  alert(formData.value.password)
 }
 
 // Form submit handler
 const onFormSubmit = () => {
   refVForm.value?.validate().then(({ valid }) => {
-    if (valid) onSubmit()
-
-    if (valid) onLogin()
+    if (valid) onSubmit() // Call onSubmit if the form is valid
   })
 }
 </script>
@@ -79,11 +65,6 @@ const onFormSubmit = () => {
     :form-success-message="formAction.formSuccessMessage"
     :form-error-message="formAction.formErrorMessage"
   ></AlertNotification>
-  <v-form class="mt-5" ref="refVForm" @submit.prevent="onFormSubmit">
-    <!-- Email Field -->
-    <v-text-field
-      v-model="formData.email"
-      label="Email"
 
   <v-form ref="refVForm" @submit.prevent="onFormSubmit">
     <!-- Email Field -->
@@ -103,10 +84,8 @@ const onFormSubmit = () => {
       v-model="formData.password"
       prepend-inner-icon="mdi-lock"
       label="Password"
-
       variant="outlined"
       density="compact"
-
       :type="isPasswordVisible ? 'text' : 'password'"
       :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
       @click:append-inner="isPasswordVisible = !isPasswordVisible"
--- PeerTutor/src/views/auth/RegisterForm.vue
+++ PeerTutor/src/views/auth/RegisterForm.vue
@@ -11,8 +11,8 @@ import { supabase, formActionDefault } from '@/utils/supabase'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
-=======
 
+// Default form data
 const formDataDefault = {
   firstname: '',
   lastname: '',
@@ -23,57 +23,23 @@ const formDataDefault = {
   occupation: ''
 }
 
-const formData = ref({
-  ...formDataDefault
-})
-const formAction = ref({
-  ...formActionDefault
-})
+// Reactive form data and action state
+const formData = ref({ ...formDataDefault })
+const formAction = ref({ ...formActionDefault })
 
+// Password visibility states
 const isPasswordVisible = ref(false)
 const isRepeatPasswordVisible = ref(false)
+
+// Reference to the form
 const refVForm = ref()
 
+// Function to handle form submission
 const onSubmit = async () => {
   formAction.value = { ...formActionDefault }
   formAction.value.formProcess = true
 
-  const { data, error } = await supabase.auth.signUp({
-    email: formData.value.email,
-    password: formData.value.password,
-    options: {
-      data: {
-        firstname: formData.value.firstname,
-        lastname: formData.value.lastname,
-        is_admin: false,
-        role: formData.value.role,
-        occupation: formData.value.occupation
-      }
-    }
-  })
-  if (error) {
-    console.log(error)
-    formAction.value.formErrorMessage = error.message
-    formAction.value.formStatus = error.status
-  } else if (data) {
-    console.log(data)
-    formAction.value.formSuccessMessage = 'Sucessfully Registered!'
-    router.replace('/home')
-  }
-  //Reset Form
-  refVForm.value?.reset()
-
-  //Turn off processing
-
-
-const isPasswordVisible = ref(false)
-const isRepeatPasswordVisible = ref(false)
-const refVForm = ref()
-
-const onSubmit = async () => {
-  formAction.value = { ...formActionDefault }
-  formAction.value.formProcess = true
-
+  // Sign up with Supabase
   const { data, error } = await supabase.auth.signUp({
     email: formData.value.email,
     password: formData.value.password,
@@ -86,18 +52,24 @@ const onSubmit = async () => {
       }
     }
   })
+
+  // Handle errors and success
   if (error) {
-    console.log(error)
+    console.error(error)
     formAction.value.formErrorMessage = error.message
     formAction.value.formStatus = error.status
   } else if (data) {
     console.log(data)
-    formAction.value.formSuccessMessage = 'Sucessfully Registered!'
-    refVForm.value?.reset()
+    formAction.value.formSuccessMessage = 'Successfully Registered!'
+    refVForm.value?.reset() // Reset the form
+    router.replace('/home') // Redirect to home
   }
+
+  // Turn off processing
   formAction.value.formProcess = false
 }
 
+// Function to validate and submit the form
 const onFormSubmit = () => {
   refVForm.value?.validate().then(({ valid }) => {
     if (valid) onSubmit()
@@ -110,10 +82,10 @@ const onFormSubmit = () => {
     :form-success-message="formAction.formSuccessMessage"
     :form-error-message="formAction.formErrorMessage"
   ></AlertNotification>
-
-  <v-form class="mt-5" ref="refVForm" @submit.prevent="onFormSubmit">
+  
+  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
+    <!-- Name Fields -->
     <v-row class="d-flex mx-auto" align="center" justify="center">
-      <!-- First Name and Last Name Fields -->
       <v-col cols="12" md="6">
         <v-text-field
           v-model="formData.firstname"
@@ -130,14 +102,17 @@ const onFormSubmit = () => {
         <v-text-field
           v-model="formData.lastname"
           label="Last Name"
+          prepend-inner-icon="mdi-account-outline"
           :rules="[requiredValidator]"
           variant="outlined"
           density="compact"
           clearable
         ></v-text-field>
       </v-col>
+    </v-row>
 
-      <!-- Email Field -->
+    <!-- Contact Information Field -->
+    <v-row class="d-flex mx-auto" align="center" justify="center">
       <v-col cols="12">
         <v-text-field
           v-model="formData.email"
@@ -149,8 +124,10 @@ const onFormSubmit = () => {
           clearable
         ></v-text-field>
       </v-col>
+    </v-row>
 
-      <!-- Password Fields -->
+    <!-- Password Fields -->
+    <v-row class="d-flex mx-auto" align="center" justify="center">
       <v-col cols="12" md="6">
         <v-text-field
           v-model="formData.password"
@@ -183,8 +160,10 @@ const onFormSubmit = () => {
           clearable
         ></v-text-field>
       </v-col>
+    </v-row>
 
-      <!-- Occupation and Role Fields -->
+    <!-- Occupation and Role Fields -->
+    <v-row class="d-flex mx-auto" align="center" justify="center">
       <v-col cols="12" md="7">
         <v-select
           v-model="formData.occupation"
@@ -228,136 +207,10 @@ const onFormSubmit = () => {
           density="compact"
         ></v-select>
       </v-col>
-
-      <!-- Submit Button -->
-      <v-container width="200" class="mb-3">
-        <v-btn
-          color="teal-darken-2"
-          size="large"
-          type="submit"
-          variant="elevated"
-          block
-          elevation="10"
-          style="border-radius: 30px"
-          :disabled="formAction.formProcess"
-          :loading="formAction.formProcess"
-        >
-          <span style="color: #80cbc4">Create</span>
-        </v-btn>
-      </v-container>
     </v-row>
-  </v-form>
 
-  <v-form ref="refVForm" @submit.prevent="onFormSubmit">
-  <!-- Name Fields -->
-  <v-row class="d-flex mx-auto" align="center" justify="center">
-    <v-col cols="12" md="6">
-      <v-text-field
-        v-model="formData.firstname"
-        label="First Name"
-        prepend-inner-icon="mdi-badge-account-outline"
-        :rules="[requiredValidator]"
-        variant="outlined"
-        density="compact"
-        clearable
-      ></v-text-field>
-    </v-col>
-    
-    <v-col cols="12" md="6">
-      <v-text-field
-        v-model="formData.lastname"
-        label="Last Name"
-        prepend-inner-icon="mdi-account-outline"
-        :rules="[requiredValidator]"
-        variant="outlined"
-        density="compact"
-        clearable
-      ></v-text-field>
-    </v-col>
-  </v-row>
-
-  <!-- Contact Information Field -->
-  <v-row class="d-flex mx-auto" align="center" justify="center">
-    <v-col cols="12">
-      <v-text-field
-        v-model="formData.email"
-        label="Email"
-        prepend-inner-icon="mdi-email"
-        :rules="[requiredValidator, emailValidator]"
-        variant="outlined"
-        density="compact"
-        clearable
-      ></v-text-field>
-    </v-col>
-  </v-row>
-
-  <!-- Password Fields -->
-  <v-row class="d-flex mx-auto" align="center" justify="center">
-    <v-col cols="12" md="6">
-      <v-text-field
-        v-model="formData.password"
-        label="Password"
-        prepend-inner-icon="mdi-lock"
-        :type="isPasswordVisible ? 'text' : 'password'"
-        :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-        @click:append-inner="isPasswordVisible = !isPasswordVisible"
-        :rules="[requiredValidator, passwordValidator]"
-        variant="outlined"
-        density="compact"
-        clearable
-      ></v-text-field>
-    </v-col>
-
-    <v-col cols="12" md="6">
-      <v-text-field
-        v-model="formData.repeatPassword"
-        label="Repeat Password"
-        prepend-inner-icon="mdi-lock-check"
-        :type="isRepeatPasswordVisible ? 'text' : 'password'"
-        :append-inner-icon="isRepeatPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
-        @click:append-inner="isRepeatPasswordVisible = !isRepeatPasswordVisible"
-        :rules="[requiredValidator, confirmedValidator(formData.repeatPassword, formData.password)]"
-        variant="outlined"
-        density="compact"
-        clearable
-      ></v-text-field>
-    </v-col>
-  </v-row>
-
-  <!-- Occupation and Role Fields -->
-  <v-row class="d-flex mx-auto" align="center" justify="center">
-    <v-col cols="12" md="7">
-      <v-select
-        v-model="formData.occupation"
-        label="Occupation"
-        :items="[
-          'Teacher', 'Tutor', 'Student', 'Professor', 'Online Instructor', 
-          'Education Consultant', 'School Administrator', 'Curriculum Developer', 
-          'Learning Facilitator', 'Private Tutor', 'Training Specialist', 
-          'Subject Matter Expert', 'Mentor', 'Coach', 'Librarian', 
-          'Research Assistant', 'Guidance Counselor', 'Special Education Teacher', 
-          'Vocational Trainer', 'Child Care Worker', 'Other'
-        ]"
-        :rules="[requiredValidator]"
-        variant="outlined"
-        density="compact"
-      ></v-select>
-    </v-col>
-
-    <v-col cols="12" md="5">
-      <v-select
-        v-model="formData.role"
-        label="Role"
-        :items="['Student', 'Tutor']"
-        :rules="[requiredValidator]"
-        variant="outlined"
-        density="compact"
-      ></v-select>
-    </v-col>
-  </v-row>
-
-  <!-- Submit Button -->
-  <v-container width="200">
+    <!-- Submit Button -->
+    <v-container width="200">
       <v-btn
         color="teal-darken-2"
         size="large"
@@ -370,6 +223,5 @@ const onFormSubmit = () => {
         Create
       </v-btn>
     </v-container>
-</v-form>
-
+  </v-form>
 </template>
