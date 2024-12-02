43bacd996712f168013aff4b8333c3c98476a6cb
Kayel2 <gulayjunkyle@gmail.com>
2024-12-02 19:08:10 +0800

    fix: fixed vercel where can access home page without signing (2)

--- PeerTutor/src/router/index.js
+++ PeerTutor/src/router/index.js
@@ -5,50 +5,54 @@ import RegisterView from '@/views/auth/RegisterView.vue';
 import HomeView from '@/views/system/HomeView.vue';
 import ProfileView from '@/views/system/ProfileView.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    redirect: () => {
+      return isAuthenticated() ? { name: 'home' } : { name: 'login' };
+    },
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView,
+    meta: { requiresAuth: true },
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      redirect: () => {
-        return isAuthenticated() ? { name: 'home' } : { name: 'login' };
-      },
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView,
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: ProfileView,
-    },
-  ],
+  routes,
 });
 
 router.beforeEach(async (to) => {
   const isLoggedIn = await isAuthenticated();
 
-  // If logged in, prevent access to login or register pages
-  if (isLoggedIn && (to.name === 'login' || to.name === 'register')) {
-    return { name: 'home' };
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    return { name: 'login' };
   }
 
-  // If not logged in and trying to access any /system page, redirect to login
-  if (!isLoggedIn && to.path.startsWith('/system')) {
-    return { name: 'login' };
+  if (isLoggedIn && (to.name === 'login' || to.name === 'register')) {
+    return { name: 'home' };
   }
 });
 
